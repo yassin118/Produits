b6b4f6bff996f17a0c30fa4fe4f8241c4f3406cb
yassin118 <haffiyessmine@gmail.com>
2022-05-07 15:11:23 +0100

    produits

---
+++ app/src/main/java/com/example/produits/Achat.java
@@ -0,0 +1,13 @@
+package com.example.produits;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Achat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_achat);
+    }
+}

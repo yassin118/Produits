a776d1ea62bc55a9563493d1732446c96bef42b0
yassin118 <haffiyessmine@gmail.com>
2022-05-08 13:30:33 +0100

    produits

--- app/src/main/java/com/example/produits/Achat.java
+++ app/src/main/java/com/example/produits/Achat.java
@@ -2,12 +2,24 @@ package com.example.produits;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class Achat extends AppCompatActivity {
+public class Achat extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_achat);
+        getSupportActionBar().hide();
+        Button btn = findViewById(R.id.confirmer);
+        btn.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this,"Achat se fait avec succées",Toast.LENGTH_SHORT).show();
     }
 }
